18c7aa4fa7a5066197d462dd1ff4572b2f494eca
Edwin Jude <edwin.judefrancis@gmail.com>
2024-07-27 01:01:57 +0530

    Programmatic navigation with navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import HomePage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -9,7 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
-import Form from "./components/Form"
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -43,11 +43,9 @@ export default function App() {
 
         {/* Parent Route and three child Route */}
         <Route path="app" element={<AppLayout />}>
+          
           {/* Defalut child route */}
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
 
           <Route
             path="cities"
@@ -62,7 +60,7 @@ export default function App() {
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
 
-          <Route path="form" element={<Form/>} />
+          <Route path="form" element={<Form />} />
         </Route>
 
         <Route path="*" element={<PageNotFound />} />
