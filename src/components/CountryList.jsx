c92568e491c53d86c231b5a451b1e216cf62e8b1
Edwin Jude <edwin.judefrancis@gmail.com>
2024-07-27 00:56:27 +0530

    Programmatic Navigation with useNavigate

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -10,19 +10,17 @@ export default function CountryList({ cities, isLoading }) {
     return (
       <Message message="Add your first city by clicking on a city on the map" />
     );
-  
-  
+
   const countries = cities.reduce((arr, city) => {
     if (!arr.map((el) => el.country).includes(city.country))
       return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
   }, []);
 
-
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
